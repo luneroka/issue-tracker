162005ea6d27b9be206db233125aad72f92cf27e
Luneroka <luneroka@gmail.com>
2025-09-24 17:33:05 +0200

    Customize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,12 @@
+import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
+import './theme-config.css';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import './globals.css';
 import NavBar from './NavBar';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -26,11 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.variable}>
       <body>
-        <Theme>
+        <Theme appearance='light' accentColor='violet'>
           <NavBar />
           <main className='p-5'>{children}</main>
+          <ThemePanel />
         </Theme>
       </body>
     </html>
