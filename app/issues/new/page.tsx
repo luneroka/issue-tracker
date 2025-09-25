0fe29456caa2618f51f6e92b028b590502ac101e
Luneroka <luneroka@gmail.com>
2025-09-25 10:31:56 +0200

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,27 @@
 'use client';
 
-import { Button, Callout, TextField } from '@radix-ui/themes';
+import { Button, Callout, Text, TextField } from '@radix-ui/themes';
 import SimpleMDEWrapper from '../../components/SimpleMDEWrapper';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod';
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState('');
 
   return (
@@ -38,6 +45,11 @@ const NewIssuePage = () => {
         <TextField.Root placeholder='Title' {...register('title')}>
           <TextField.Slot />
         </TextField.Root>
+        {errors.title && (
+          <Text color='red' as='p'>
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name='description'
           control={control}
@@ -45,7 +57,11 @@ const NewIssuePage = () => {
             <SimpleMDEWrapper placeholder='Description' {...field} />
           )}
         />
-
+        {errors.description && (
+          <Text color='red' as='p'>
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
