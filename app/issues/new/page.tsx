0654feebfba080f7803f4acc79127ba05ea6375d
Luneroka <luneroka@gmail.com>
2025-09-25 09:58:27 +0200

    Handled form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,41 @@
+'use client';
+
 import { Button, TextField } from '@radix-ui/themes';
-import React from 'react';
 import SimpleMDEWrapper from '../../components/SimpleMDEWrapper';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className='max-w-xl space-y-3'>
-      <TextField.Root placeholder='Title'>
+    <form
+      className='max-w-xl space-y-3'
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/issues', data);
+        router.push('/issues');
+      })}
+    >
+      <TextField.Root placeholder='Title' {...register('title')}>
         <TextField.Slot />
       </TextField.Root>
-      <SimpleMDEWrapper placeholder='Description' />
+      <Controller
+        name='description'
+        control={control}
+        render={({ field }) => (
+          <SimpleMDEWrapper placeholder='Description' {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
